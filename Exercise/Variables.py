3de78b60780c071482b78f314e0301b595119953
Jitendra Parmar <69849843+Repidex@users.noreply.github.com>
2020-10-16 13:45:26 +0530

    Add files via upload
    Variable type and printing the values of variable.

---
+++ Exercise/Variables.py
@@ -0,0 +1,16 @@
+# Code starts here
+age=12
+name="John"
+# Assign v1ariable names
+type_age=age
+type_name=name
+
+# Check types
+print(type(type_age))          #Print the type of variables
+print(type(type_name))
+
+# print out types
+print(type_age)
+print(type_name)
+
+# Code ends here
